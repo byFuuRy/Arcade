f2e886f11ebd5dc02590ab7cf186fecc900686fc
BuildTools <florent.piccinini@epitech.eu>
2019-03-19 14:21:24 +0100

    Add constructors for Vector, Rect, Color

---
+++ arcade/common/src/Color.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** Arcade
+** File description:
+** Color
+*/
+
+#include "../include/Color.hpp"
+
+Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) noexcept
+{
+    this->r = r;
+    this->g = g;
+    this->b = b;
+    this->a = a;
+}
